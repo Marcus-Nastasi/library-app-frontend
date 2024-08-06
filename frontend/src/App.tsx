8c8ca8706ffedb8f8a4c7bb87fa73cadd59c3b5a
rolemberg <vinnie.nsts@gmail.com>
2024-08-06 11:50:36 -0300

    consuming searching books by name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,9 @@ import Button from "./components/Button.tsx";
 
 export default function App() {
    const [ books, setBooks ] = useState<Array<IBook> | undefined>();
+   const [ isBooks, setIsBooks ] = useState<boolean>();
+   const [ booksByName, setBooksByName ] = useState<Array<IBook> | undefined>();
+   const [ isBooksByName, setIsBooksByName ] = useState<boolean>();
 
    useEffect(() => {
       // fetching books
@@ -21,6 +24,8 @@ export default function App() {
             });
             const response: Array<IBook> = await request.json();
             setBooks(response);
+            setIsBooksByName(false);
+            setIsBooks(true);
          } catch(e: any) {
             console.log(e);
          }
@@ -28,22 +33,46 @@ export default function App() {
       getAllBooks();
    }, []);
 
+   const searchByName = async (e: any): Promise<void> => {
+      e.preventDefault();
+      const url: string = `http://localhost:8080/api/books/search/name`;
+      const name: any = document.getElementById('searchByName');
+      const token: string | null = Cookie.getCookie('libToken');
+      if (token === null) window.open('/login', '_self');
+      try {
+         const request: Response = await fetch(url, {
+            method: 'post',
+            body: JSON.stringify({ name: name.value }),
+            headers: new Headers({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` })
+         });
+         const books: Array<IBook> = await request.json();
+         setIsBooks(false);
+         setIsBooksByName(true);
+         setBooksByName(books);
+      } catch(e: any) {
+         console.log(e.message);
+      }
+   };
+
    return (
       <div className={`flex flex-col justify-center items-center min-h-screen max-h-fit overflow-x-hidden`}>
          <div className="w-screen min-h-screen max-h-fit p-10 pt-1 flex flex-wrap justify-evenly items-center bg-neutral-100">
             <div className={`p-10`}>
                <input
+                  id="searchByName"
                   type={'search'}
                   placeholder={'search books...'}
                   className={'p-0.5 px-2 mr-2 border-t-2 border-l-2 border-r-2 border-b-2 border-t-neutral-400 border-l-neutral-400 border-b-neutral-800 border-r-neutral-600 focus:outline-none'}
                   style={{ textIndent: '0px' }}
                />
                <Button 
-                  text={'Search'} 
+                  text={'Search'}
+                  funct={searchByName}
                />
             </div>
 
-            { books && books.map((b: IBook) => <BookCard {...b} />) || 'loading...' }
+            { isBooks && books && books.map((b: IBook) => <BookCard {...b} />) || 'loading...' }
+            { isBooksByName && booksByName && booksByName.map((b: IBook) => <BookCard {...b} />) }
          </div>
       </div>
    );
